6acf16027832a311eb7286c8943f783b068dcc0c
Azatul <nurulazatul2791@gmail.com>
2025-08-22 14:56:10 +0800

    Update user profile changeset: add gender field to cast and validation requirements for improved user data management.

--- lib/spato/accounts/user_profile.ex
+++ lib/spato/accounts/user_profile.ex
@@ -24,8 +24,8 @@ defmodule Spato.Accounts.UserProfile do
   @doc false
   def changeset(user_profile, attrs) do
     user_profile
-    |> cast(attrs, [:user_id, :department_id, :full_name, :dob, :ic_number, :position, :employment_status, :date_joined, :profile_picture_url, :last_seen_at, :address, :phone_number])
-    |> validate_required([:full_name, :dob, :ic_number, :position, :employment_status, :date_joined, :profile_picture_url, :last_seen_at])
+    |> cast(attrs, [:user_id, :department_id, :full_name, :dob, :ic_number, :position, :gender, :employment_status, :date_joined, :profile_picture_url, :last_seen_at, :address, :phone_number])
+    |> validate_required([:full_name, :dob, :ic_number, :position, :gender, :employment_status, :date_joined, :profile_picture_url, :last_seen_at])
   end
 
   def human_employment_status(:full_time), do: "Sepenuh Masa"
